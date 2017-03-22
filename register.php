55d001cb31301c54824d98e372c64e432b858a7c
mlichocki <mlichocki@me.com>
2017-03-22 17:29:20 -0400

    Update register.php

--- register.php
+++ register.php
@@ -6,13 +6,13 @@
 	$password = $_POST["password"];
 	$email = $_POST["email"];
 
-	$query = "SELECT * FROM users WHERE BINARY username = '$username';";
+	/*$query = "SELECT * FROM users WHERE BINARY username = '$username';";
 	$result = mysqli_query($connect, $query);
 
 	if(mysqli_num_rows($result) > 0){
 		echo json_encode(array("status" => "inuse"));
 	}
-	else{
+	else{*/
 		$query = "INSERT INTO users (username, password, email) VALUES ('$username', '$password', '$email')";
 		$result = mysqli_query($connect, $query);
 		
@@ -22,7 +22,7 @@
 		else{
 			echo json_encode(array("status" => "fail"));
 		}
-	}
+	//}
 
 	$connect->close();
 
