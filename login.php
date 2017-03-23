8b93c3e4bdd0a94f2daafa3927ad469b805b31b7
mlichocki <mlichocki@me.com>
2017-03-22 21:34:04 -0400

    Update login.php

--- login.php
+++ login.php
@@ -13,7 +13,7 @@
 	//If the user is found
 	if(mysqli_num_rows($result) ==1){
 		$row = mysqli_fetch_assoc($result);
-		echo json_encode(array("role" => $row["role"]));
+		echo json_encode(array("role" => $row["role"], "guardian" => $row["guardian"]));
 		mysqli_close($connect);
 
 	}
