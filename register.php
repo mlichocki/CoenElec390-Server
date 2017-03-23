c92a76d2b9d791830ce6a2f678bc357691aacec1
mlichocki <mlichocki@me.com>
2017-03-22 20:07:27 -0400

    Update register.php

--- register.php
+++ register.php
@@ -20,7 +20,7 @@
 		
 		if($result){
 			// sql to create table
-			$query = "CREATE TABLE $username (
+			$query = "CREATE TABLE Testtable (
 			`id` INT NOT NULL,
 			`name` TEXT(45) NOT NULL,
 			`username` VARCHAR(45) NOT NULL,
