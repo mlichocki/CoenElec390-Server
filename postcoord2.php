272c0211fd99a8fb592aa9dbe0061cf91bc8afe9
mlichocki <mlichocki@me.com>
2017-03-27 12:59:27 -0400

    Update postcoord2.php

--- postcoord2.php
+++ postcoord2.php
@@ -12,9 +12,9 @@
 
 	if($result){
 		while($row = mysqli_fetch_assoc($result)){
-			$query = "INSERT INTO `"$row["username"]"` (latitude, longitude) VALUES ($latitude, $longitude);";
+			$query = "INSERT INTO "$row["username"]" (latitude, longitude) VALUES ($latitude, $longitude);";
 			$result = mysqli_query($connect, $query);
-			echo json_encode(array("role" => $row["role"]));
+			echo json_encode(array("role" => $row["id"]));
 			mysqli_close($connect);
 		}
 	}
