a26e77d48b9bfc701adc716e69b0d98448a09a22
mlichocki <mlichocki@me.com>
2017-03-27 11:33:32 -0400

    Update addchild2.php

--- addchild2.php
+++ addchild2.php
@@ -29,7 +29,14 @@
 			$query = "INSERT INTO ".$guardianUsername." (username, name) VALUES ('$childUsername', '$name');";
 			$result1 = mysqli_query($connect, $query);
 
-			$query = "INSERT INTO ".$childUsername." (username) VALUES ('$guardianUsername');";
+			//$query = "INSERT INTO ".$childUsername." (username) VALUES ('$guardianUsername');";
+			$query = "CREATE TABLE `$childUsername` (
+			`id` INT NOT NULL AUTO_INCREMENT,
+			`username` VARCHAR(45) NOT NULL DEFAULT `$guardianUsername`,
+			`BeaconLatitude` DOUBLE NULL DEFAULT NULL,
+			`BeaconLongitude` DOUBLE NULL DEFAULT NULL,
+			`BeaconRadius` DOUBLE NULL DEFAULT NULL,
+			PRIMARY KEY (`id`))";
 			$result2 = mysqli_query($connect, $query);
 			if($result1 && $result2){
 				echo json_encode(array("status" => "added"));
