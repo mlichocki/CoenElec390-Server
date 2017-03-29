4c0b974f61b32085851c740aae7dfb1a5500ba7d
mlichocki <mlichocki@me.com>
2017-03-28 23:35:25 -0400

    Update postcoord2.php

--- postcoord2.php
+++ postcoord2.php
@@ -3,7 +3,6 @@
 	require "connect.php";
 
 	$username = $_POST['username'];
-	$guardian = $_POST['guardian'];
 	$latitude = $_POST['latitude'];
 	$longitude = $_POST['longitude'];
 
@@ -12,11 +11,10 @@
 
 	if($result){
 		while($row = mysqli_fetch_assoc($result)){
-			$query = "UPDATE `$row["username"]` SET latitude = '$latitude', longitude = '$longitude' WHERE username = '$username';";
+			$query = "UPDATE '"$row["username"]"' SET latitude = '$latitude', longitude = '$longitude' WHERE username = '$username';";
 			$result = mysqli_query($connect, $query);
-			echo json_encode(array("role" => $row["username"]));
-			mysqli_close($connect);
 		}
+		mysqli_close($connect);
 	}
 	else{
 	}
