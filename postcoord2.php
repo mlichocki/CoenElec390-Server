1fdd8bd319459ffea58dcfe78bb13f4c2c9f2c00
mlichocki <mlichocki@me.com>
2017-03-28 23:44:49 -0400

    Update postcoord2.php

--- postcoord2.php
+++ postcoord2.php
@@ -10,10 +10,11 @@
 	$result = mysqli_query($connect, $query);
 
 	if($result){
-		while($row = mysqli_fetch_assoc($result)){
+		//while($row = mysqli_fetch_assoc($result)){
+$row = mysqli_fetch_assoc($result)
 			$query = "UPDATE `$row['username']` SET latitude = '$latitude', longitude = '$longitude' WHERE username = '$username';";
 			$result2 = mysqli_query($connect, $query);
-		}
+		//}
 		mysqli_close($connect);
 	}
 	else{
