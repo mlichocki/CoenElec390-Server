043c55be3e6efeeaaedc3ee944a31cad89c38ead
mlichocki <mlichocki@me.com>
2017-03-29 11:09:04 -0400

    Update postcoord.php

--- postcoord.php
+++ postcoord.php
@@ -12,20 +12,20 @@
 	if(mysqli_num_rows($result) > 0){
 		while($row = mysqli_fetch_assoc($result)){
 			$tablename = $row["username"];
-			/*
+			
 			if(($row["BeaconLatitude"] != null) && ($row["BeaconLongitude"] != null) && ($row["BeaconRadius"] != null)){ 
-				$distanceToBeacon = ((($row["BeaconLatitude"] - $latitude)**2) + (($row["BeaconLongitude"] - $longitude)**2))**(1/2);
-				if($distanceToBeacon <= $row["BeaconRadius"]){
+				/*$distanceToBeacon = ((($row["BeaconLatitude"] - $latitude)**2) + (($row["BeaconLongitude"] - $longitude)**2))**(1/2);
+				if($distanceToBeacon <= $row["BeaconRadius"]){*/
 					$notification = TRUE;
-				}
+				/*}
 				else{
 					$notification = FALSE;
-				}
+				}*/
 			}
 			else{
 				$notification = FALSE;
 			}
-			*/
+			
 			$query = "UPDATE `$tablename` SET latitude = '$latitude', longitude = '$longitude' WHERE username = '$username';";
 			$result2 = mysqli_query($connect, $query);
 		}
