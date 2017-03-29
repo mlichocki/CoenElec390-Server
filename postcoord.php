23fe47acbf214e8a5472af1507a16760a17ef4be
mlichocki <mlichocki@me.com>
2017-03-29 11:17:27 -0400

    Update postcoord.php

--- postcoord.php
+++ postcoord.php
@@ -14,8 +14,8 @@
 			$tablename = $row["username"];
 			
 			if(($row["BeaconLatitude"] != null) && ($row["BeaconLongitude"] != null) && ($row["BeaconRadius"] != null)){ 
-				/*$distanceToBeacon = ((($row["BeaconLatitude"] - $latitude)**2) + (($row["BeaconLongitude"] - $longitude)**2))**(1/2);
-				if($distanceToBeacon <= $row["BeaconRadius"]){*/
+				$distanceToBeacon = ((($row["BeaconLatitude"] - $latitude)**2) + (($row["BeaconLongitude"] - $longitude)**2))**(1/2);
+				/*if($distanceToBeacon <= $row["BeaconRadius"]){*/
 					$notification = TRUE;
 				/*}
 				else{
