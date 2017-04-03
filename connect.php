558807c30787a1701fba0b71f825aece4d290031
mlichocki <mlichocki@me.com>
2017-04-03 18:25:21 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,11 +3,7 @@
 	$username = "coenelec390";
 	$password = "Cn6BC?M?6x65";
 	$host = "mysql4.gear.host";
-	$dbname = "coenelec390$";
+	$dbname = "coenelec390";
 	$connect = mysqli_connect($host, $username, $password, $dbname);
-
-	if(!$connect){
-		echo ("CONNECTION TO SERVER FAILED!")
-	}
 
 ?>
